d8b6b0b5ea4a0ffdae6e58a3f9d5d922e4b5c24f
renee <rnxbiz@hotmail.com>
2025-04-25 11:48:38 +0800

    slice

--- dev.go
+++ dev.go
@@ -5,19 +5,34 @@ import (
 )
 
 func main() {
-	whatAmI := func(i interface{}) {
-		switch t := i.(type) {
-		case bool:
-			fmt.Println("I'm a bool")
-		case int:
-			fmt.Println("I'm an int")
-		default:
-			fmt.Printf("Don't know type %T\n", t)
-		}
+	var s []string
+	fmt.Println("uninit:", "!", s == nil, s, len(s) == 0)
+	s = make([]string, 3)
+	fmt.Println("uninit:", "!", s == nil, s, len(s) == 3)
 
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	s = append(s, "d")
+	s = append(s, "f")
+	fmt.Println("apd:", s, "len:", len(s))
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy", c)
+
+	l := s[2:5]
+	fmt.Println("s2:5", l, "len:", len(l))
+
+	l = s[:5]
+	fmt.Println("s:5", l, "len:", len(l))
+
+	l = s[2:]
+	fmt.Println("s2:", l, "len:", len(l))
+	var a int = 10
+	fmt.Printf("&a1=%p\n", &a)
+	{
+		a := 10
+		fmt.Printf("&a2=%p\n", &a)
 	}
-
-	whatAmI(true)
-	whatAmI(1)
-	whatAmI("hey")
 }
